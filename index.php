0a4f63e35db6e14da7e6e2123dcf57adc72c9eca
Przemysław Kustoń <qprzemo@gmail.com>
2017-01-17 16:48:57 +0100

    Skonczony projekt

--- index.php
+++ index.php
@@ -1,48 +1,155 @@
-<!doctype html>
-<html class="no-js" lang="">
+﻿<!doctype html>
+<html class="no-js" lang="pl">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>ABC</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PK & DG</title>
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
+		<!-- Bootstrap Core CSS -->
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+		<!-- Custom Fonts -->
+		<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+		<!-- Plugin CSS -->
+		<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+		<!-- Theme CSS -->
+		<link href="css/creative.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+		
 
+<script type="text/javascript" src="http://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="http://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script type="text/javascript">
+    window.onload = function() {
+        var dataPoints = [];
+ 
+function getDataPointsFromCSV(csv) {
+    var dataPoints = csvLines = points = [];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);
+        
+    for (var i = 0; i < csvLines.length; i++)
+        if (csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            dataPoints.push({ 
+                x: parseFloat(points[0]), 
+                y: parseFloat(points[1]) 		
+	    });
+	}
+    return dataPoints;
+}
+	 
+	$.get("/oesk2lab/gallery_JPG/jpg.csv", function(data) {
+		var chart = new CanvasJS.Chart("chartContainer", {
+			animationEnabled: true,
+			backgroundColor: null,
+			title: {
+			text: "JPG",
+			},
+			data: [{
+			type: "column",
+			dataPoints: getDataPointsFromCSV(data)
+		}]
+		});
+			
+		chart.render();
+	});
+
+    $.get("/oesk2lab/gallery_svg/svg.csv", function(data) {
+        var chart2 = new CanvasJS.Chart("chartContainer2", {
+            animationEnabled: true,
+            backgroundColor: null,
+            title: {
+                text: "SVG",
+            },
+            data: [{
+                type: "column",
+                dataPoints: getDataPointsFromCSV(data)
+            }]
+        });
+
+        chart2.render();
+    });
+
+    $.get("/oesk2lab/gallery_URI/uri.csv", function(data) {
+        var chart3 = new CanvasJS.Chart("chartContainer3", {
+            animationEnabled: true,
+            backgroundColor: null,
+            title: {
+                text: "URI",
+            },
+            data: [{
+                type: "column",
+                dataPoints: getDataPointsFromCSV(data)
+            }]
+        });
+
+        chart3.render();
+    });
+    }
+	</script>
     </head>
-    <body>
-
-     <!-- Add your site or application content here -->
-        <p>Projekt zaliczewniowy z OESK, Przemysław Kustoń i Dawid Głośny</p>
-<table width="744" border="1">
-    <tr><th height="41" scope="row">
-            <a href="index.php?option=upload" style="margin-left:100px">Upload Image</a>
-            <a href="index.php?option=gallery" style="margin-left:10px">Image gallery</a>
-        </th></tr>
-    <tr>
-        <th height="401" scope="row">
-            <?php
-            @$gall=$_GET['option'];
-                switch ($gall)
-                {
-                    case 'upload':
-                        include ('upload.php');
-                        break;
-                    case 'gallery':
-                        include ('gallery.php');
-                        break;
-                }
-
+   <body id="page-top" class="bcg1">
+			    
+	<section id="services">
+        <div class="container">
+            <div class="row">
+			<div class="col-lg-12 text-center">
+                <p>Projekt zaliczeniowy OESK - Przemysław Kustoń, Dawid Głośny</p>
+				<a href="index.php?option=upload" class="btn btn-primary btn-xl page-scroll">Testy i pomiary</a>
+				<a href="index.php?option=gallery" class="btn btn-primary btn-xl page-scroll">Galeria</a>
+			 </div>	
+			
+                <div class="col-lg-7 text-left">
+                    <h2 class="section-heading">&nbsp</h2>
+					  <?php
+						@$gall=$_GET['option'];
+							switch ($gall)
+						{
+							case 'upload':
+								include ('upload.php');
+								break;
+							case 'gallery':
+								include ('gallery.php');
+								break;
+						}
             ?>
-        </th>
-    </tr>
-</table>
-
+			 <h2 class="section-heading">&nbsp</h2>
+                </div>
+                <div class="col-lg-5 text-center">
+                    <h2 class="section-heading">&nbsp</h2>
+                    <p>Podgląd stron testowych</p>
+                    <div class="col-lg-4 text-center"><a href="static/JPG.php">JPG</a></div>
+                    <div class="col-lg-4 text-center"><a href="static/SVG.php">SVG</a></div>
+                    <div class="col-lg-4 text-center"><a href="static/URI.php">URI</a></div>
+                </div>
+				<div class="col-lg-12 text-center">
+                    <h2 class="section-heading">&nbsp</h2>
+                    <p>Wyniki</p>
+                    <div class="col-lg-4 text-center">
+                    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+                    </div>
+                        <div class="col-lg-4 text-center">
+                    <div id="chartContainer2" style="height: 300px; width: 100%;"></div>
+                        </div>
+                            <div class="col-lg-4 text-center">
+                    <div id="chartContainer3" style="height: 300px; width: 100%;"></div>
+                            </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
--- upload.php
+++ upload.php
@@ -1,125 +1,197 @@
 <?php
-if(isset($_POST['upd']))
-{
-    if(file_exists("gallery/".$_FILES['file']['name']))
-    {
-        echo "<font color='red'>".$_FILES['file']['name']." already exists</font>";
-    }
-    else{
+if (isset($_POST['upd'])) {
+    if (file_exists("gallery/" . $_FILES['file']['name'])) {
+        echo "<font color='red'>" . $_FILES['file']['name'] . " Obraz był już dodany, przetestowałem obecne galerie</font>";
+    } else {
         $supported_image = array('jpg', 'jpeg', 'png', 'bmp', 'svg');
         $src_file_name = $_FILES['file']['name'];
         $ext = strtolower(pathinfo($src_file_name, PATHINFO_EXTENSION));
 
-    //    $imageData = base64_encode(file_get_contents($src_file_name));
         $info = pathinfo($src_file_name);
-        $file_name =  basename($src_file_name,'.'.$info['extension']);
+        $file_name = basename($src_file_name, '.' . $info['extension']);
 
-        if (in_array($ext, $supported_image))
-        {
-            move_uploaded_file($_FILES['file']['tmp_name'], "gallery/".$_FILES['file']['name']);
-            copy("gallery/".$_FILES['file']['name'],"gallery_JPG/".$_FILES['file']['name']);
+        if (in_array($ext, $supported_image)) {
+            move_uploaded_file($_FILES['file']['tmp_name'], "gallery/" . $_FILES['file']['name']);
+            copy("gallery/" . $_FILES['file']['name'], "gallery_JPG/" . $_FILES['file']['name']);
             echo " Zdjęcie załadowane do galerii rastrowej";
             echo "<hr>";
             exec("java -jar ImageTracer.jar gallery/$src_file_name outfilename gallery_SVG/$file_name.svg");
             echo " Zdjęcie przekonwertowane na SVG";
 
             echo "<hr>";
-            $img_string = 'data:image/' . $ext . ';base64,' . base64_encode(file_get_contents("gallery/".$_FILES['file']['name']));
+            $img_string = 'data:image/' . $ext . ';base64,' . base64_encode(file_get_contents("gallery/" . $_FILES['file']['name']));
             echo " Zdjęcie przekonwertowane do dataURI";
             echo "<hr>";
-            file_put_contents("gallery_URI/".$file_name.".txt",$img_string);
-            $data_uri = file_get_contents('gallery_URI/'.$file_name.".txt", FILE_USE_INCLUDE_PATH);
-
-
-        }
-            else{
+            file_put_contents("gallery_URI/" . $file_name . ".txt", $img_string);
+            $data_uri = file_get_contents('gallery_URI/' . $file_name . ".txt", FILE_USE_INCLUDE_PATH);
+        } else {
             echo "<font color='red'>please choose a valid image</font>";
-            }
+        }
     }
 
     $jpg = exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/JPG.php');
     $svg = exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/SVG.php');
     $uri = exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/URI.php');
 
+	$jpg_pomiar = $jpg;
+	$svg_pomiar = $svg;
+	$uri_pomiar = $uri;
+
     $enter = "\n";
-// otwarcie pliku do odczytu
     $fp = fopen("gallery_JPG/wyniki_jpg.txt", "r");
-//odczytanie danych
     $stareDane = fread($fp, filesize("gallery_JPG/wyniki_jpg.txt"));
-// zamknięcie pliku
     fclose($fp);
-// stworzenie nowych danych
-//    $stareDane .= $enter;
     $jpg .= $enter;
     $jpg .= $stareDane;
-// otwarcie pliku do zapisu
     $fp = fopen("gallery_JPG/wyniki_jpg.txt", "w");
-// zapisanie danych
     fputs($fp, $jpg);
-// zamknięcie pliku
     fclose($fp);
 
-// otwarcie pliku do odczytu
     $fp = fopen("gallery_SVG/wyniki_svg.txt", "r");
-//odczytanie danych
     $stareDane = fread($fp, filesize("gallery_SVG/wyniki_svg.txt"));
-// zamknięcie pliku
     fclose($fp);
-// stworzenie nowych danych
+    $svg .= $enter;
     $svg .= $stareDane;
-// otwarcie pliku do zapisu
     $fp = fopen("gallery_SVG/wyniki_svg.txt", "w");
-// zapisanie danych
     fputs($fp, $svg);
-// zamknięcie pliku
     fclose($fp);
 
-// otwarcie pliku do odczytu
     $fp = fopen("gallery_URI/wyniki_uri.txt", "r");
-//odczytanie danych
     $stareDane = fread($fp, filesize("gallery_URI/wyniki_uri.txt"));
-// zamknięcie pliku
     fclose($fp);
-// stworzenie nowych danych
+    $uri .= $enter;
     $uri .= $stareDane;
-
-// otwarcie pliku do zapisu
     $fp = fopen("gallery_URI/wyniki_uri.txt", "w");
-// zapisanie danych
     fputs($fp, $uri);
-// zamknięcie pliku
     fclose($fp);
 
 
     echo "<hr>";
-    echo " Pomiary stron:";
+    echo " Najnowszy pomiar:<br />";
     echo " JPG: ";
-    echo exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/JPG.php');
-    echo "<hr>";
-    echo " SVG: ";
-    echo exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/SVG.php');
-    echo "<hr>";
-    echo " dataURI: ";
-    echo exec('phantomjs loadspeed.js http://localhost/oesk2lab/static/URI.php');
-}
+    echo $jpg_pomiar;
+    echo "<br /> SVG: ";
+    echo $svg_pomiar;
+    echo "<br /> dataURI: ";
+    echo $uri_pomiar;
 
 
+    //TWORZENIE PLIKÓW CSV DLA WYKRESÓW - JPG
+	$count = 1;
+    $items = array();
+	if ($data = fopen("gallery_JPG/wyniki_jpg.txt", "r")) {
+        while (!feof($data)) {
+            $line = fgets($data);
+            $whatIWant = between('Loading time ', ' msec', $line);
+            $items[] = $count.",".$whatIWant;
+            $count++;
+        }
+	fclose($data);
+	// print_r($items); wypisanie listy
+
+        $file = fopen("gallery_jpg/jpg.csv","w");
+        foreach ($items as $line)
+        {
+            fputcsv($file,explode(',',$line));
+        }
+        fclose($file);
+    }
+
+    //TWORZENIE PLIKÓW CSV DLA WYKRESÓW - SVG
+    $count = 1;
+    $items = array();
+    if ($data = fopen("gallery_SVG/wyniki_svg.txt", "r")) {
+        while (!feof($data)) {
+            $line = fgets($data);
+            $whatIWant = between('Loading time ', ' msec', $line);
+            $items[] = $count.",".$whatIWant;
+            $count++;
+        }
+        fclose($data);
+      //   print_r($items); Wypisanie listy
+
+        $file = fopen("gallery_svg/svg.csv","w");
+        foreach ($items as $line)
+        {
+            fputcsv($file,explode(',',$line));
+        }
+        fclose($file);
+    }
+
+    //TWORZENIE PLIKÓW CSV DLA WYKRESÓW - JPG
+    $count = 1;
+    $items = array();
+    if ($data = fopen("gallery_URI/wyniki_uri.txt", "r")) {
+        while (!feof($data)) {
+            $line = fgets($data);
+            $whatIWant = between('Loading time ', ' msec', $line);
+            $items[] = $count.",".$whatIWant;
+            $count++;
+        }
+        fclose($data);
+     //   print_r($items); Wypisanie listy
+
+        $file = fopen("gallery_uri/uri.csv","w");
+        foreach ($items as $line)
+        {
+            fputcsv($file,explode(',',$line));
+        }
+        fclose($file);
+    }
+}//KONIEC FUNKCJI NA GUZIK
+
+
+  
+echo "<hr>";
+echo "<br />Wszystkie wyniki:";
 
 if ($data = fopen("gallery_JPG/wyniki_jpg.txt", "r")) {
-    while(!feof($data)) {
-        echo "<hr>";
-        echo "<hr>";
+    echo "<br />JPG(ms) ";
+    while (!feof($data)) {
         $line = fgets($data);
-        $whatIWant = substr($line, strpos($line, "Loading time ") + 13);
-        echo $whatIWant;
+        $whatIWant = between('Loading time ', ' msec', $line);
+        echo $whatIWant . "\n";
+    }
+    fclose($data);
+}
+
+if ($data = fopen("gallery_SVG/wyniki_svg.txt", "r")) {
+    echo "<br />SVG(ms) ";
+    while (!feof($data)) {
+        $line = fgets($data);
+        $whatIWant = between('Loading time ', ' msec', $line);
+        echo $whatIWant . "\n";
+    }
+    fclose($data);
+}
+
+if ($data = fopen("gallery_URI/wyniki_uri.txt", "r")) {
+    echo "<br />URI(ms) ";
+    while (!feof($data)) {
+        $line = fgets($data);
+        $whatIWant = between('Loading time ', ' msec', $line);
+        echo $whatIWant . "\n";
     }
     fclose($data);
 }
 
 
+//Zestaw funkcji wyciągających wyniki pomiarów z pliku txt
+function after($this1, $inthat) {
+    if (!is_bool(strpos($inthat, $this1)))
+        return substr($inthat, strpos($inthat, $this1) + strlen($this1));
+};
+
+function before($this1, $inthat) {
+    return substr($inthat, 0, strpos($inthat, $this1));
+};
+
+function between($this1, $that, $inthat) {
+    return before($that, after($this1, $inthat));
+};
+
 ?>
 
-<form method="post" enctype="multipart/form-data">
-    Wybierz obraz<input type="file" name="file" /><hr><br/>
-    <input type="submit" value="Załaduj" name="upd"/>
+<form method="post" enctype="multipart/form-data"><br/>Wybierz obraz<input type="file" name="file" /><hr><br/>
+    <input type="submit" class="btn btn-primary btn-xl page-scroll" value="Wykonaj test i zbierz wyniki" name="upd"/>
 </form>
